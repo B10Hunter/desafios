570ff8497da880d420af8f4393882c736942a2af
Lucas Albornoz <flipskate@outlook.es>
2022-07-04 12:27:41 -0300

    sumador de numero

--- desafio1/js/main.js
+++ desafio1/js/main.js
@@ -1,27 +1,20 @@
-let numero 
-let traductor =true
-while (numero = (prompt("Traductor de numero 1 al 10 a ingles"))) {
-    switch (traductor) {
-        case "1": alert("one")
-         traductor =false
-         case "2": alert("two")
-         traductor =false
-         case "3": alert("three")
-         traductor =false
-         case "4": alert("four")
-         traductor =false
-         case "5": alert("fived")
-         traductor =false
-         case "6": alert("six")
-         traductor =false
-         case "7": alert("seven")
-         traductor =false
-         case "8": alert("eight")
-         traductor =false
-         case "9": alert("nine")
-         traductor =false
-         case "10": alert("ten")
-         traductor =false
-    }
+const respuesta = (numero1, numero2) => numero1 + numero2
+
+let numero1 , numero2 , alerta
+
+do {
+    alerta = alert ("sumador de numero")
+    numero1 =  parseFloat(prompt("ingrese un numero"))
+    numero2 = parseFloat(prompt("ingrese otro numero"))
     
-}
+    if (isNaN (numero1) || isNaN (numero2) ){
+        alert ("Por favor no ingrese letras, solo numeros")
+    }
+    if (numero2) { alert 
+        (respuesta (numero1, numero2) ) 
+    
+    }
+
+    
+    
+} while (isNaN(numero1) || isNaN (numero2))   
