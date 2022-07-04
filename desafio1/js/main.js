8b3ed06d8c49360d45189e4ab7e0e9b6a763ea22
Lucas Albornoz <flipskate@outlook.es>
2022-07-04 02:47:45 -0300

    fin de bucle

---
+++ desafio1/js/main.js
@@ -0,0 +1,27 @@
+let numero 
+let traductor =true
+while (numero = (prompt("Traductor de numero 1 al 10 a ingles"))) {
+    switch (traductor) {
+        case "1": alert("one")
+         traductor =false
+         case "2": alert("two")
+         traductor =false
+         case "3": alert("three")
+         traductor =false
+         case "4": alert("four")
+         traductor =false
+         case "5": alert("fived")
+         traductor =false
+         case "6": alert("six")
+         traductor =false
+         case "7": alert("seven")
+         traductor =false
+         case "8": alert("eight")
+         traductor =false
+         case "9": alert("nine")
+         traductor =false
+         case "10": alert("ten")
+         traductor =false
+    }
+    
+}
