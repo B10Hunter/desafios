f2485a9055df91b10ce58e75f4340015e2a7e008
Lucas Albornoz <flipskate@outlook.es>
2022-07-14 15:11:56 -0300

    "pre-entrego"

--- desafio1/js/main.js
+++ desafio1/js/main.js
@@ -1,20 +1,50 @@
-const respuesta = (numero1, numero2) => numero1 + numero2
+let main = alert(" |<._.>| Bienvenido a la calculadora interactiva") + alert ("Te voy hacer dos pequeñas preguntas antes de seguir :D")
 
-let numero1 , numero2 , alerta
+class Usuario {
+    constructor (nombre, edad){
+        this.nombre = nombre
+        this.edad = edad
+        if(isNaN (edad)){
+            alert("Por favor solo numeros.");
+            } 
+          
+            else {
+                alert (" Tu nombre es "+ nombre + (" y tu edad es de " + edad ) ) + alert (nombre + " gracias por responder :D"); 
+            }
+            }
+    } 
+
+const usurario1 = new Usuario (prompt("Ingrese tu nombre."), parseInt(prompt("ingresa tu edad.")))
+console.log (usurario1);
+let calculadora = alert("Empecemos a calcular |<._.>|" ) 
+
+const suma = (num1,num2) => num1 + num2
+const resta = (num1,num2) => num1 - num2
+const multiplicar = (num1,num2) => num1 * num2
+const dividir = (num1,num2) => num1 / num2
+
+let numero1, numero2, operacion
 
 do {
-    alerta = alert ("sumador de numero")
-    numero1 =  parseFloat(prompt("ingrese un numero"))
-    numero2 = parseFloat(prompt("ingrese otro numero"))
-    
-    if (isNaN (numero1) || isNaN (numero2) ){
-        alert ("Por favor no ingrese letras, solo numeros")
-    }
-    if (numero2) { alert 
-        (respuesta (numero1, numero2) ) 
-    
-    }
+    numero1 = parseFloat(prompt("|<._.>| Ingrese un numero ")) 
+    numero2 = parseFloat(prompt("|<._.>| Ingrese otro numero"))
+    operacion = prompt("|<._.>| Ingrese una operacion (+, -, *, /)")
+}while(isNaN(numero1) || isNaN(numero2))
 
-    
-    
-} while (isNaN(numero1) || isNaN (numero2))   
+switch(operacion) {
+    case "+":
+        alert( "|<._.>| Tu resultado es " + numero1 + numero2);
+        break;
+    case "-":
+        alert(  "|<._.>| Tu resultado es " +  numero1 - numero2)
+        break
+    case "*":
+        alert(  "|<._.>| Tu resultado es " +  numero1 * numero2)
+        break
+    case "/":
+        alert(  "|<._.>| Tu resultado es " +  numero1 / numero2)
+        break
+    default:
+        alert("Operacion no valida")
+        break
+}
